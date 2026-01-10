1a8850b0fe5d8e258056f057388ef7b5065ad29d
kimsowon <sowon836@gmail.com>
2026-01-11 01:29:31 +0900

    feat: 공통 컴포넌트 헤더 (#21)
    - pnpm 설치
    - 로고.svg 적용
    - 헤더구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import Header from '@/components/layout/Header'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,9 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
+      <body className={`antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
