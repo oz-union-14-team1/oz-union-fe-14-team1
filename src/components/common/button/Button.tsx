114ba45e004e1f20b44dfef7b78d5606aa56c8dd
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2026-01-13 13:23:33 +0900

    feat: 버튼컴포넌트 수정 (#20)

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -24,7 +24,11 @@ const CustomButton = ({
     <button
       {...props}
       className={cn(
+<<<<<<< HEAD
         'flex h-[2.4375rem] w-[4.375rem] items-center justify-center rounded-md font-medium transition-all',
+=======
+        'flex h-10 w-17.5 items-center justify-center rounded-md font-medium transition-all',
+>>>>>>> 803ea82 (feat: 버튼컴포넌트 수정 (#20))
         BUTTON_VARIANTS[variant], // 선택된 variant 스타일 적용
         className // 외부에서 주는 추가 클래스
       )}
