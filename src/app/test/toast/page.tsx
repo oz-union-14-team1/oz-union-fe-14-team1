a6d860054ae9e60fe3cf4277e6a74642fe2cd460
jaemin_123 <61594883+lollonoaJoro@users.noreply.github.com>
2026-01-13 14:02:59 +0900

    feat: 공통컴포넌트 버튼제작 (#20)
    * feat: 버튼컴포넌트 수정 (#20)
    
    * feat: pmpn-lick 파일 충돌 해결 (#20)
    
    * feat: 버튼컴포넌트 수정 (#20)
    
    * feat: 공통 컴포넌트 토스트 제작 (#20)
    
    * feat: 토스트컴포넌트 수정 (#20)
    
    * feat: 버튼컴포넌트 수정 (#20)
    
    * fix: 코드리뷰반영 컴플리트 해결 (#20)

---
+++ src/app/test/toast/page.tsx
@@ -0,0 +1,9 @@
+import Toast from '@/components/common/toast/Toast'
+
+export default function Home() {
+  return (
+    <div>
+      <Toast type="error" message="성공했습니다!" />
+    </div>
+  )
+}
---
+++ src/components/common/toast/Toast.tsx
@@ -0,0 +1,50 @@
+type ToastVariant = 'error' | 'success' | 'warning'
+
+interface ToastProps {
+  type: ToastVariant
+  message: string
+}
+const TOAST_VARIANTS: Record<
+  ToastVariant,
+  { container: string; iconBg: string; icon: string }
+> = {
+  error: {
+    container:
+      'bg-[var(--color-toast-error)]/10 border-[var(--color-toast-error)] text-[var(--color-toast-error)]',
+    iconBg: 'bg-[var(--color-toast-error)]',
+    icon: '✕',
+  },
+  success: {
+    container:
+      'bg-[var(--color-toast-success)]/10 border-[var(--color-toast-success)] text-[var(--color-toast-success)]',
+    iconBg: 'bg-[var(--color-toast-success)]',
+    icon: '✓',
+  },
+  warning: {
+    container:
+      'bg-[var(--color-toast-warning)]/10 border-[var(--color-toast-warning)] text-[var(--color-toast-warning)]',
+    iconBg: 'bg-[var(--color-toast-warning)]',
+    icon: '!',
+  },
+}
+
+const Toast = ({ type, message }: ToastProps) => {
+  const style = TOAST_VARIANTS[type]
+
+  return (
+    <div
+      className={`flex w-fit min-w-[340px] items-center gap-3 rounded-xl border px-5 py-3 shadow-md ${style.container}`}
+    >
+      {/* 아이콘 영역 */}
+      <div
+        className={`flex h-8 w-8 items-center justify-center rounded-full font-bold text-white ${style.iconBg} ${style.iconBg}`}
+      >
+        {style.icon}
+      </div>
+
+      {/* 메시지 영역 */}
+      <span className="text-[15px] font-medium">{message}</span>
+    </div>
+  )
+}
+export default Toast
