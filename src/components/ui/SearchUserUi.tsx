0e8a7be2ec6c592f2ef1c426764ee6af3c1f4bcd
kimsowon <sowon836@gmail.com>
2026-01-13 11:22:13 +0900

    fix: edit SearchUserUi size (#21)
    - 검색아이콘, 유저아이콘 사이즈 수정

--- src/components/ui/SearchUserUi.tsx
+++ src/components/ui/SearchUserUi.tsx
@@ -3,7 +3,7 @@ import { Search, CircleUser } from 'lucide-react'
 
 export default function SearchUserUi() {
   return (
-    <div className="flex size-8 shrink-0 items-center justify-center gap-4 text-text-light">
+    <div className="flex shrink-0 items-center justify-center gap-4 text-text-light">
       <Link href="/search">
         <Search className="size-8 text-current transition-colors duration-200 hover:text-main-purple md:hidden" />
       </Link>
