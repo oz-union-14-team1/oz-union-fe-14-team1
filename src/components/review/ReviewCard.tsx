af1e04359e5e6b2832048b9a0d28204e71a730a6
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2026-01-14 14:06:33 +0900

    fix: ReviewCard 수정 (#59)

---
+++ src/components/review/ReviewCard.tsx
@@ -0,0 +1,52 @@
+import { cn } from '@/utils'
+
+function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        'flex flex-col rounded-[var(--radius-lg)] border border-white/5 text-[var(--color-text-light)] shadow-sm',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardHeader({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn('flex items-center justify-between p-6 pb-0', className)}
+      {...props}
+    />
+  )
+}
+
+function CardContent({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn('p-6 pt-2 text-[var(--color-text-light)]', className)}
+      {...props}
+    />
+  )
+}
+
+function CardFooter({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn('flex justify-end p-6 pt-0', className)} {...props} />
+  )
+}
+
+Card.Header = CardHeader
+Card.Content = CardContent
+Card.Footer = CardFooter
+
+export { Card }
