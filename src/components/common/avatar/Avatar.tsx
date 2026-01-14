9f75b192b5630ee7f2211a6dadd0cbaba721b6f8
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2026-01-14 19:14:20 +0900

    feat: 아바타코드 수정 (#68)

--- src/components/common/avatar/Avatar.tsx
+++ src/components/common/avatar/Avatar.tsx
@@ -1,8 +1,7 @@
-import { ComponentProps } from 'react'
-
-type AvatarProps = ComponentProps<'div'> & {
+import Image from 'next/image'
+type AvatarProps = React.ComponentProps<'div'> & {
   src?: string | null
-  alt?: string
+  alt: string
   fallback?: string
 }
 
@@ -13,14 +12,6 @@ export default function Avatar({
   className,
   ...props
 }: AvatarProps) {
-  return (
-    <div
-      className={`h-[50px] w-[50px] shrink-0 overflow-hidden rounded-full bg-gray-200 ${className}`}
-      {...props}
-    >
-      {src ? (
-        <img src={src} alt={alt} className="h-full w-full object-cover" />
-      ) : null}
-    </div>
-  )
+  const imageNode = src ? <Image src={src} alt={alt} /> : null
+  return <>{imageNode}</>
 }
