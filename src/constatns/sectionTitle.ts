8d13d6d166d1dcbe6caea436267270e3dc4ad581
Sammylee <sammul95@gmail.com>
2026-01-14 10:24:17 +0900

    feat: section-title 컴포넌트 구현 (#61)
    - SectionTitle 공통 컴포넌트 구현
    - 섹션 타이틀 상수 파일 생성
    - 컴포넌트 테스트 페이지 생성

---
+++ src/constatns/sectionTitle.ts
@@ -0,0 +1,18 @@
+export const SECTION_TITLES = {
+  NEW_RELEASE: {
+    title: '새로운 게임을 발견하세요',
+    href: '/games/new-release',
+  },
+  RECOMMENDATION: {
+    title: '선호기반 추천 리스트',
+    href: 'games/recommend',
+  },
+  MYFAVORITES: {
+    title: '찜한 게임과 비슷한 추천',
+    href: '/games/my-favorites',
+  },
+  GENREGAME: {
+    title: '찜한 게임과 비슷한 추천',
+    href: '/games/genre',
+  },
+} as const
