de0932fe22c05f5447375745fbd57cbcfc53bc22
LeeSaemMool <sammul95@gmail.com>
2026-01-15 01:03:44 +0900

    fix: section title 폴더이동, genrecard, gamecard 수정 (#63)
    - SectionTitle 폴더 구조 변경
    - GenreCard 오타 수정
    - GameCard 반응형 작업을 위해 크기값 및 일부 스타일 수정
    - pnpm-lockfile 린트 처리

--- src/components/common/game-card/GameCard.tsx
+++ src/components/common/game-card/GameCard.tsx
@@ -2,17 +2,13 @@
 
 import { Heart } from 'lucide-react'
 import Image from 'next/image'
-import { StaticImageData } from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 
+import { Game } from '@/types/game'
 import { cn } from '@/utils'
 
-type GameCardProps = {
-  id: string
-  name: string
-  imgUrl: string | StaticImageData
-}
+type GameCardProps = Game & {}
 
 export function GameCard({ id, name, imgUrl }: GameCardProps) {
   const [isWishlisted, setIsWishlisted] = useState(false)
@@ -27,7 +23,7 @@ export function GameCard({ id, name, imgUrl }: GameCardProps) {
     <Link href={`/game/${id}`} className="justify-start">
       <div
         className={cn(
-          'relative h-90 w-54 bg-background pt-2 transition-all duration-300',
+          'relative w-full bg-background/40 pt-2 transition-all duration-300',
           'flex flex-col items-center justify-start',
           'hover:scale-105 hover:shadow-2xl',
           'group'
@@ -40,27 +36,29 @@ export function GameCard({ id, name, imgUrl }: GameCardProps) {
           className={cn(
             'absolute top-4 right-4 z-10 size-8 cursor-pointer rounded-full bg-black/40 transition-all',
             'flex flex-col items-center justify-center',
-            'hover:bg-black/60',
+            'hover:bg-white/20',
             isWishlisted ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
           )}
         >
           <Heart
             className={cn(
               'size-5 transition-colors',
-              isWishlisted ? 'fill-red-500 text-red-500' : 'text-white'
+              isWishlisted
+                ? 'fill-main-purple text-btn-main-active'
+                : 'text-white'
             )}
           />
         </button>
-        <div className="flex w-full flex-col items-start justify-center gap-5 px-2 text-text-light">
+        <div className="relative mb-3 aspect-3/4 w-full overflow-hidden">
           <Image
             src={imgUrl}
             alt={name}
-            width={200}
-            height={270}
-            className="h-auto w-full"
+            fill
+            sizes="(max-width: 768px) 50vw, (max-width:1024px) 25vw, 16vw"
+            className="object-cover"
           />
-          <h1 className="text-lg font-bold">{name}</h1>
         </div>
+        <p className="text-lg font-bold text-text-light">{name}</p>
       </div>
     </Link>
   )
