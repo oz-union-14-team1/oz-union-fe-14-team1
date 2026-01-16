32a1e59371816cb4298390abfc0991e8a867ce43
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2026-01-16 13:43:56 +0900

    fix: textarea onCancel onSubmit 추가수정 (#79)

--- src/components/review/Textarea.tsx
+++ src/components/review/Textarea.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as React from 'react'
 
 import Button from '@/components/common/button/Button'
@@ -7,21 +9,36 @@ export const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.TextareaHTMLAttributes<HTMLTextAreaElement>
 >(({ className, ...props }, ref) => {
+  const [isOpen, setIsOpen] = React.useState(true)
+
+  const onCancel = () => {
+    setIsOpen(false)
+  }
+
+  const onSubmit = () => {
+    console.log('실행')
+  }
+
+  if (!isOpen) {
+    return null
+  }
+
   return (
     <div className="rounded-lg border bg-text-dark p-3 focus-within:ring-1 focus-within:ring-purple-500">
       <textarea
         ref={ref}
+        {...props}
         className={cn(
-          'min-h-45 w-full resize-none bg-transparent text-sm text-white placeholder-gray-500 outline-none',
+          'min-h-[120px] w-full resize-none bg-transparent text-xl text-white placeholder-gray-500 outline-none',
           className
         )}
-        {...props}
       />
 
-      {/* 하단 버튼 영역 */}
       <div className="mt-2 flex justify-end gap-2">
-        <Button />
-        <Button variant="outline" />
+        <Button onClick={onSubmit}>등록</Button>
+        <Button variant="outline" onClick={onCancel}>
+          취소
+        </Button>
       </div>
     </div>
   )
