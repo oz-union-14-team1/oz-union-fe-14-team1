998de0006df684a5a62b146876f0d563d69cff42
kimsowon <sowon836@gmail.com>
2026-01-16 13:38:16 +0900

    Bug: GameCard 사이즈 수정 (#81)
    * fix: bug: edit GameCard size (#81)
    
    - 게임카드에 들어갈 이미지의 사이즈가 다르더라도 게임카드의 사이즈는 고정되도록 수정
    
    * fix: bug: edit GameCard size (#81)
    
    - 게임카드안에 들어가는 이미지의 사이즈가 달라지더라도 게임카드의 사이즈가 고정되도록 수정

--- src/app/test/game-card/page.tsx
+++ src/app/test/game-card/page.tsx
@@ -1,77 +1,15 @@
-import { StaticImageData } from 'next/image'
-
-import Game1Image from '@/assets/images/games/game1.png'
-import Game2Image from '@/assets/images/games/game2.png'
 import { GameCard } from '@/components/common/game-card'
-
-type GameCardData = {
-  id: string
-  name: string
-  imgUrl: string | StaticImageData
-}
-
-export const GAMES: GameCardData[] = [
-  {
-    id: '1',
-    name: 'Kingdom Come: Deliverance II',
-    imgUrl: Game1Image,
-  },
-  {
-    id: '2',
-    name: "Sid Meier's Civilization® VII",
-    imgUrl: Game2Image,
-  },
-  {
-    id: '3',
-    name: 'Kingdom Come: Deliverance II',
-    imgUrl: Game1Image,
-  },
-  {
-    id: '4',
-    name: "Sid Meier's Civilization® VII",
-    imgUrl: Game2Image,
-  },
-  {
-    id: '5',
-    name: 'Kingdom Come: Deliverance II',
-    imgUrl: Game1Image,
-  },
-  {
-    id: '6',
-    name: "Sid Meier's Civilization® VII",
-    imgUrl: Game2Image,
-  },
-  {
-    id: '7',
-    name: 'Kingdom Come: Deliverance II',
-    imgUrl: Game1Image,
-  },
-  {
-    id: '8',
-    name: "Sid Meier's Civilization® VII",
-    imgUrl: Game2Image,
-  },
-  {
-    id: '9',
-    name: 'Kingdom Come: Deliverance II',
-    imgUrl: Game1Image,
-  },
-  {
-    id: '10',
-    name: "Sid Meier's Civilization® VII",
-    imgUrl: Game2Image,
-  },
-]
+import { MOCK_GAMES } from '@/mocks'
 
 export default function GameCardTestPage() {
   return (
     <div className="mx-auto flex max-w-(--width-container) flex-wrap justify-start gap-2">
-      {GAMES.map((game) => (
+      {MOCK_GAMES.map((game) => (
         <GameCard
           key={`grid-${game.id}`}
           id={game.id}
           name={game.name}
-          imgUrl={game.imgUrl as StaticImageData}
+          imgUrl={game.imgUrl}
         />
       ))}
     </div>
