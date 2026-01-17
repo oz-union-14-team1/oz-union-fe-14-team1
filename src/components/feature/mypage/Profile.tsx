1ff2ff69b4f47c9f45eba9ed7e812ba8ed837c9e
kimsowon <sowon836@gmail.com>
2026-01-17 16:44:53 +0900

    feat: compose mypage profile section (#92)
    - 프로필 구성 컴포넌트 추가
    - feature 인덱스 연결

---
+++ src/components/feature/mypage/Profile.tsx
@@ -0,0 +1,56 @@
+import {
+  EditProfileImageUi,
+  GenresTagUi,
+  GlowUi,
+  ProfileBackgroundUi,
+  ProfileImageUi,
+  ProfileNameDesktopUi,
+  ProfileNameMobileUi,
+} from './ui'
+
+export default function Profile() {
+  return (
+    <>
+      <div className="relative">
+        <GlowUi />
+        <div className="group relative flex h-40.75 w-40.75 cursor-pointer flex-col items-center justify-center md:h-auto md:w-auto">
+          <EditProfileImageUi />
+          <ProfileImageUi />
+          <ProfileNameMobileUi />
+          <GenresTagUi
+            genre="GENRES (MOBILE)"
+            variant="main"
+            className="absolute z-7 mt-80 block px-4 py-2 text-sm md:hidden"
+          />
+          <div className="absolute left-1/2 z-7 flex w-[45vw] -translate-x-1/2 translate-y-[0%] flex-wrap items-center justify-center gap-2 pt-120 md:hidden">
+            {/* 🔴🔴🔴 API 🔴🔴🔴 */}
+            {['ACTION', 'RPG', 'ADVENTURE', 'SHOOTER'].map((genre) => (
+              <GenresTagUi
+                key={genre}
+                genre={genre}
+                className="px-3 py-1.5 text-xs"
+              />
+            ))}
+          </div>
+        </div>
+        <ProfileNameDesktopUi />
+        <GenresTagUi
+          genre="GENRES (DESKTOP)"
+          variant="main"
+          className="absolute z-2 hidden md:top-15.25 md:left-38 md:block"
+        />
+        <ProfileBackgroundUi />
+        <div className="absolute top-30 left-41 z-2 hidden flex-wrap items-center gap-2 md:flex">
+          {/* 🔴🔴🔴 API 🔴🔴🔴 */}
+          {['ACTION', 'RPG', 'ADVENTURE', 'SHOOTER'].map((genre) => (
+            <GenresTagUi
+              key={genre}
+              genre={genre}
+              className="px-4 py-1.5 text-sm"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/feature/mypage/index.ts
@@ -0,0 +1,10 @@
+// src/components/feature/mypage/index.ts
+export { default as Profile } from './Profile'
+// ui
+export { default as GenresTagUi } from './ui/GenresTagUi'
+export { default as GlowUi } from './ui/GlowUi'
+export { default as EditProfileImageUi } from './ui/EditProfileImageUi'
+export { default as ProfileImageUi } from './ui/ProfileImageUi'
+export { default as ProfileNameMobileUi } from './ui/ProfileNameMobileUi'
+export { default as ProfileNameDesktopUi } from './ui/ProfileNameDesktopUi'
+export { default as ProfileBackgroundUi } from './ui/ProfileBackgroundUi'
