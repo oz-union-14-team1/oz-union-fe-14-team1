2f82f4cc11ebb316ae854bd9de8992d5e62b446f
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-18 15:07:26 +0900

    refactor: 토스트 컴포넌트 cva 및 테일윈드 스타일 변수 적용 (#99)

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,50 +1,58 @@
-type ToastVariant = 'error' | 'success' | 'warning'
+import { cva, VariantProps } from 'class-variance-authority'
 
-export interface ToastProps {
-  type: ToastVariant
+import { cn } from '@/utils'
+
+type ToastType = 'success' | 'warning' | 'error'
+
+const TOAST_ICON: Record<ToastType, string> = {
+  success: '✓',
+  warning: '!',
+  error: '✕',
+} as const
+
+const TOAST_ICON_BG_COLOR: Record<ToastType, string> = {
+  success: 'bg-toast-success',
+  warning: 'bg-toast-warning',
+  error: 'bg-toast-error',
+} as const
+
+const ToastVariant = cva(
+  cn(
+    'flex w-fit min-w-85 items-center gap-3 rounded-xl border px-5 py-3 shadow-md'
+  ),
+  {
+    variants: {
+      type: {
+        success:
+          'bg-toast-success/10 border border-toast-success  text-toast-success',
+        warning:
+          'bg-toast-warning/10 border border-toast-warning text-toast-warning',
+        error: 'bg-toast-error/10 border border-toast-error text-toast-error',
+      },
+    },
+  }
+)
+
+export interface ToastProps extends VariantProps<typeof ToastVariant> {
+  type: ToastType
   message: string
 }
 
-const TOAST_VARIANTS: Record<
-  ToastVariant,
-  { container: string; iconBg: string; icon: string }
-> = {
-  error: {
-    container:
-      'bg-[var(--color-toast-error)]/10 border-[var(--color-toast-error)] text-[var(--color-toast-error)]',
-    iconBg: 'bg-[var(--color-toast-error)]',
-    icon: '✕',
-  },
-  success: {
-    container:
-      'bg-[var(--color-toast-warning)]/10 border-[var(--color-toast-warning)] text-[var(--color-toast-warning)]',
-    iconBg: 'bg-[var(--color-toast-warning)]',
-    icon: '!',
-  },
-  warning: {
-    container:
-      'bg-[var(--color-toast-success)]/10 border-[var(--color-toast-success)] text-[var(--color-toast-success)]',
-    iconBg: 'bg-[var(--color-toast-success)]',
-    icon: '✓',
-  },
-}
-
 const Toast = ({ type, message }: ToastProps) => {
-  const style = TOAST_VARIANTS[type]
-
   return (
-    <div
-      className={`flex w-fit min-w-[340px] items-center gap-3 rounded-xl border px-5 py-3 shadow-md ${style.container}`}
-    >
+    <div className={cn(ToastVariant({ type }))}>
       {/* 아이콘 영역 */}
       <div
-        className={`flex h-8 w-8 items-center justify-center rounded-full font-bold text-white ${style.iconBg} ${style.iconBg}`}
+        className={cn(
+          'flex h-8 w-8 items-center justify-center rounded-full font-bold text-white',
+          TOAST_ICON_BG_COLOR[type]
+        )}
       >
-        {style.icon}
+        {TOAST_ICON[type]}
       </div>
 
       {/* 메시지 영역 */}
-      <span className="text-[15px] font-medium">{message}</span>
+      <span className="text-base font-medium">{message}</span>
     </div>
   )
 }
