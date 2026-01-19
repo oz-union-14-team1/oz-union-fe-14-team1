5ef6190304bc6227c75a5f20294e3e2501acb182
kimsowon <sowon836@gmail.com>
2026-01-19 17:25:22 +0900

    chore: 리팩토링에 따른 export 경로 업데이트 (#103)
    - mobile/desktop 폴더 구조에 맞춰 index.ts 파일 수정
    - 통합된 컴포넌트 export 제거
    - 분리된 컴포넌트 export 추가

--- src/components/feature/mypage/ui/index.ts
+++ src/components/feature/mypage/ui/index.ts
@@ -1,8 +1,10 @@
 // src/components/feature/mypage/ui/index.ts
-export { default as GenresTagUi } from './GenresTagUi'
-export { default as GlowUi } from './GlowUi'
 export { default as EditProfileImageUi } from './EditProfileImageUi'
 export { default as ProfileImageUi } from './ProfileImageUi'
-export { default as ProfileNameMobileUi } from './ProfileNameMobileUi'
-export { default as ProfileNameDesktopUi } from './ProfileNameDesktopUi'
 export { default as ProfileBackgroundUi } from './ProfileBackgroundUi'
+// desktop
+export { default as GenresTagDesktopUi } from './desktop/GenresTagDesktopUi'
+export { default as ProfileDesktopUi } from './desktop/ProfileDesktopUi'
+// mobile
+export { default as GenresTagMobileUi } from './mobile/GenresTagMobileUi'
+export { default as ProfileMobileUi } from './mobile/ProfileMobileUi'
