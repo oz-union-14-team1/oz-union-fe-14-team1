cf1540b937a799d1e0b7e8c8c4685416e30704e9
웅배의 깃헙 <jeonwb622@gmail.com>
2026-01-19 14:41:26 +0900

    refactor: 회원가입 페이지 로고 이미지 부분 코드 이동 (#111)
    - SignupForm에 있는 로고 부분을 page로 이동
    - SignupForm 배럴패턴 적용

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,18 @@
-import SignupForm from '@/components/feature/auth/SignupForm'
+import Image from 'next/image'
 
+import { compoundLogoRow } from '@/assets'
+import { SignupForm } from '@/components'
+
+/**
+ * 회원가입 페이지 Image 로고 부분 + Form 부분
+ */
 export default function SignupPage() {
-  return <SignupForm />
+  return (
+    <div className="flex w-full flex-col gap-5">
+      <div className="mb-2 flex justify-center">
+        <Image src={compoundLogoRow} alt="PlayTypeLogo" priority />
+      </div>
+      <SignupForm />
+    </div>
+  )
 }
