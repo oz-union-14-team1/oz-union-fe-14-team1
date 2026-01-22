c692436837e8e95102a6170d070356a0fed68c99
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-22 14:22:04 +0900

    fix: MSW가 실행 된 이후 앱 실행되도록 변경 (#141)

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,22 +1,29 @@
 'use client'
 
-import { ReactNode, useEffect } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 import { ToastProvider } from '@/components/providers/ToastProvider'
-
-export async function initMSW() {
-  const { worker } = await import('@/mocks/browser')
-  await worker.start()
-}
 
 interface ProvidersProps {
   children: ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
+  const [isMSWReady, setIsMSWReady] = useState(false)
+
   useEffect(() => {
+    const initMSW = async () => {
+      const { worker } = await import('@/mocks/browser')
+      await worker.start()
+      setIsMSWReady(true)
+    }
+
     initMSW()
   }, [])
+
+  if (!isMSWReady) {
+    return null
+  }
 
   return <ToastProvider>{children}</ToastProvider>
 }
