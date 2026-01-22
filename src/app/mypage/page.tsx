006436e72c14531da9a852f9b03a2ebb60e87c3d
kimsowon <sowon836@gmail.com>
2026-01-22 16:42:03 +0900

    feat: 마이페이지 메인 화면 구성 및 전역 스타일 업데이트 (#136)
    - 마이페이지에 DashBoard 및 MyReviewList 컴포넌트 배치
    - globals.css에 폰트 설정 및 공통 스타일 업데이트

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,14 +1,12 @@
-import { DashBoard, Profile, WishList } from '@/components/feature/mypage'
+import { DashBoard, MyReviewList, Profile, WishList } from '@/components/feature/mypage'
 
-// test 폴더에서는 반응형 확인이 어려워서 실제페이지에서 작업하며 확인했습니다.
-// ================================================================ //
 export default function MyPage() {
   // TODO: 실제 API에서 데이터를 가져와서 사용
   const WISHLIST_COUNT = 15
   const REVIEW_COUNT = 8
 
   return (
-    <section className="container mx-auto max-w-345">
+    <section className="container mx-auto mb-50 max-w-345">
       <div className="flex flex-col items-center justify-between gap-10 px-5 pt-20 lg:flex-row lg:items-start lg:gap-5">
         <div className="w-full lg:w-1/2">
           <Profile imageUrl="" />
@@ -24,7 +22,9 @@ export default function MyPage() {
       <div className="mt-10 px-0 lg:mt-20">
         <WishList />
       </div>
+      <div className="my-10 px-5">
+        <MyReviewList />
+      </div>
     </section>
   )
 }
-// ================================================================ //
