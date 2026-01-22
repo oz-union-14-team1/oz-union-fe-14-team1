0f9e6d850f44c19c6edd8d79e8c1798ec01b3b73
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-22 14:14:30 +0900

    chore: setup msw (#141)
    - client 컴포넌트를 위한 browser worker
    - server 컴포넌트를 위한 server worker

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,11 +1,22 @@
-import { ReactNode } from 'react'
+'use client'
+
+import { ReactNode, useEffect } from 'react'
 
 import { ToastProvider } from '@/components/providers/ToastProvider'
+
+export async function initMSW() {
+  const { worker } = await import('@/mocks/browser')
+  await worker.start()
+}
 
 interface ProvidersProps {
   children: ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
+  useEffect(() => {
+    initMSW()
+  }, [])
+
   return <ToastProvider>{children}</ToastProvider>
 }
