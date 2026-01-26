bb926e34c38cdc124d216a06008cb39e3ae2d824
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-26 15:01:21 +0900

    fix: api path 상수에 MSW_BASE_URL 제거 (#164)

--- src/constants/apiPath.ts
+++ src/constants/apiPath.ts
@@ -2,5 +2,5 @@ export const MSW_BASE_URL = 'https://msw/api/v1'
 
 export const API_PATH = {
   REVIEW_DETAIL_API_PATH: (review_id: string) =>
-    `${MSW_BASE_URL}/community/reviews/${review_id}/comments`,
+    `/community/reviews/${review_id}/comments`,
 } as const
