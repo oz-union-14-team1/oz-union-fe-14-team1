de05e16b6cec6c0c7833a8907a2f41b5c0edb486
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-26 13:22:34 +0900

    chore: api 인스턴스 생성 (#161)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,9 @@
+import api from '@/utils/axios'
+
 // src/utils/index.ts
 export * from './cn'
 export { searchStorage } from './searchStorage'
 export { shuffle } from './shuffle'
 export * from './rotation'
 export { getTagVariant } from './getTagVariant'
+export { api }
