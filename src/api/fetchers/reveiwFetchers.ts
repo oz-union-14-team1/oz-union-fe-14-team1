87e79168e6673a7eae1c11b8d7dfa9caf3b83797
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-26 15:58:38 +0900

    fix: getReviewDetail에 MSW_BASE_URL 추가 (#164)

--- src/api/fetchers/reveiwFetchers.ts
+++ src/api/fetchers/reveiwFetchers.ts
@@ -1,10 +1,10 @@
-import { API_PATH } from '@/constants/apiPath'
+import { API_PATH, MSW_BASE_URL } from '@/constants/apiPath'
 import { ReviewDetail } from '@/types/api-response/review-response'
 import { api } from '@/utils'
 
 export const getReviewDetail = async (reviewId: string) => {
   const res = await api.get<ReviewDetail>(
-    API_PATH.REVIEW_DETAIL_API_PATH(reviewId)
+    `${MSW_BASE_URL}${API_PATH.REVIEW_DETAIL_API_PATH(reviewId)}`
   )
 
   return res.data
