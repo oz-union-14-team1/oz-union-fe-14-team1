7a7ba86c1202e0e74ef449f3b890c831090ee2f6
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-28 16:03:04 +0900

    fix: msw 리뷰 삭제 / 수정 path parameter 수정 (#181)
    - comment_id -> review_id

--- src/mocks/handlers/reviewHandlers.ts
+++ src/mocks/handlers/reviewHandlers.ts
@@ -20,7 +20,7 @@ const getReviewDetail = http.get(
 )
 
 const putReview = http.patch(
-  `${MSW_BASE_URL}${API_PATH.REVIEW_PATCH_DELETE_API_PATH(`:comment_id`)}`,
+  `${MSW_BASE_URL}${API_PATH.REVIEW_PATCH_DELETE_API_PATH(`:review_id`)}`,
   async ({ request }) => {
     const { content } = (await request.clone().json()) as { content: string }
 
@@ -33,7 +33,7 @@ const putReview = http.patch(
 )
 
 const deleteReview = http.delete(
-  `${MSW_BASE_URL}${API_PATH.REVIEW_PATCH_DELETE_API_PATH(`:comment_id`)}`,
+  `${MSW_BASE_URL}${API_PATH.REVIEW_PATCH_DELETE_API_PATH(`:review_id`)}`,
   () => HttpResponse.json({}, { status: 204 })
 )
 
