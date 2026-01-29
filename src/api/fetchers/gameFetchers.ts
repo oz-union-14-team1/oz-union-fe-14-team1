f3fa971dc18a1ba63ad5692a3b2cbe024a3a1737
SammyLee <sammul95@gmail.com>
2026-01-29 16:07:35 +0900

    refactor: game API 전용 axios instance 분리  (#196)
    * refactor: camel api axios instance 분리 (#195)
    
    - camelCase 변환 interceptor가 적용된 camelApi 신규 생성
    - 신규 도메인 API(게임, 인증 등)에서 camelApi 사용 가능하도록 구조 분리
    
    * refactor: 공통 interceptor 분리 및 camelApi 적용 구조 개선 (#195)
    
    - 공통 response interceptor 로직을attachDefaultResponseInterceptor로 분리
    - camelApi에 interceptor 추가
    - axios instance 설정 구조 정리 및 중복 제거

--- src/api/fetchers/gameFetchers.ts
+++ src/api/fetchers/gameFetchers.ts
@@ -4,17 +4,17 @@ import {
   GameFilterParams,
   GameList,
 } from '@/types/api-response/game-response'
-import { api } from '@/utils'
+import { camelApi } from '@/utils/axios'
 
 export const getGames = async (params?: GameFilterParams) => {
-  const res = await api.get<GameList>(`${MSW_BASE_URL}${API_PATH.GAMES}`, {
+  const res = await camelApi.get<GameList>(`${MSW_BASE_URL}${API_PATH.GAMES}`, {
     params,
   })
   return res.data
 }
 
 export const getGameDetail = async (gameId: number) => {
-  const res = await api.get<GameDetail>(
+  const res = await camelApi.get<GameDetail>(
     `${MSW_BASE_URL}${API_PATH.GAME_DETAIL(gameId)}`
   )
   return res.data
