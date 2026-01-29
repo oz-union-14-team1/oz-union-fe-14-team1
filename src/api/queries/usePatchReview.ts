a0683a9bb3cb05497e200a37d68e78ae7d3a5c00
jhyon123@gmail.com <ljh@JaeHyunLees-MacBook-Air.local>
2026-01-29 14:42:35 +0900

    feat: 리뷰 수정 결과 핸들링 (#181)

--- src/api/queries/usePatchReview.ts
+++ src/api/queries/usePatchReview.ts
@@ -1,7 +1,9 @@
 import { useMutation, UseMutationOptions } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
 
 import { patchReview } from '@/api/fetchers/reveiwFetchers'
+import { useToast } from '@/hooks'
 
 type PatchReviewOption = Omit<
   UseMutationOptions<
@@ -13,8 +15,21 @@ type PatchReviewOption = Omit<
 >
 
 export default function usePatchReview(options?: PatchReviewOption) {
+  const { triggerToast } = useToast()
+  const router = useRouter()
+
   return useMutation({
     mutationFn: patchReview,
+    onSuccess: () => {
+      triggerToast('success', '리뷰를 성공적으로 수정했습니다.')
+      router.refresh()
+    },
+    onError: () => {
+      triggerToast(
+        'error',
+        '리뷰 수정에 실패했습니다. 잠시후 다시 시도해주세요.'
+      )
+    },
     ...options,
   })
 }
