81271c6b8a6dfe82645b7337700c9436aab067f0
LeeSaemMool <sammul95@gmail.com>
2026-02-06 11:11:41 +0900

    refactor: mock게임 데이터구조 api 스펙에 맞게 수정 (#211)
    - mockGameList / mockGameDetail 타입 구조 API 응답 형식에 맞게 수정 및 분리
    - mcokGame.ts 제거
    - gameHandler list, detail로 분리
    - mock/index.ts export 정리

--- src/mocks/handlers/gameHandlers.ts
+++ src/mocks/handlers/gameHandlers.ts
@@ -1,55 +1,29 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse, passthrough } from 'msw'
 
-import { API_PATH, MSW_BASE_URL } from '@/constants/apiPath'
-import { MOCK_GAMES } from '@/mocks/data/mockGames'
+import { MOCK_GAME_DETAIL } from '@/mocks/data/mockGameDetail'
+import { MOCK_GAME } from '@/mocks/data/mockGameList'
 
 export const gameHandlers = [
-  http.get(`${MSW_BASE_URL}${API_PATH.GAMES}`, ({ request }) => {
+  http.get('*/game/', ({ request }) => {
     const url = new URL(request.url)
-    const genreId = url.searchParams.get('genre_id')
-    const tagId = url.searchParams.get('tag_id')
-    const year = url.searchParams.get('year')
-    // const minScore = url.searchParams.get('min_score')
     const page = Number(url.searchParams.get('page') ?? 1)
 
-    /**
-     * TEMP:
-     * 현재 MSW 단계에서만 사용하는 클라이언트 필터 로직
-     * 백엔드 필터 API 적용 시 제거 예정
-     */
-    let filteredGames = [...MOCK_GAMES]
+    const useMock = url.searchParams.get('mock')
 
-    if (genreId) {
-      const genreIds = genreId.split(',').map(String)
-      filteredGames = filteredGames.filter((game) =>
-        game.genres?.some((id) => genreIds.includes(id))
-      )
-    }
-
-    if (tagId) {
-      const tagIds = tagId.split(',').map(String)
-      filteredGames = filteredGames.filter((game) =>
-        game.genres?.some((id) => tagIds.includes(id))
-      )
-    }
-
-    if (year) {
-      const years = year.split(',').map(Number)
-      filteredGames = filteredGames.filter((game) => {
-        const gameYear = new Date(game.releasedAt!).getFullYear()
-        return years.includes(gameYear)
+    if (useMock === 'true') {
+      return HttpResponse.json({
+        page,
+        results: MOCK_GAME,
       })
     }
+    return passthrough()
+  }),
 
-    // if (minScore) {
-    //   filteredGames = filteredGames.filter(
-    //     (game) => (game. ?? 0) >= Number(minScore)
-    //   )
-    // }
+  http.get('*/game/:id', ({ params }) => {
+    const { id } = params
 
-    return HttpResponse.json({
-      page,
-      results: filteredGames,
-    })
+    const game = MOCK_GAME_DETAIL.find((g) => g.id === Number(id))
+
+    return HttpResponse.json(game ?? MOCK_GAME_DETAIL[0])
   }),
 ]
