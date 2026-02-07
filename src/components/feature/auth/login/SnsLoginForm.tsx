7c05076b099b94e3c206ab2e79694fc4640859ca
웅배의 깃헙 <jeonwb622@gmail.com>
2026-02-07 21:00:09 +0900

    feat: 구글 소셜 로그인 구현
    구글 소셜 로그인 구현

--- src/components/feature/auth/login/SnsLoginForm.tsx
+++ src/components/feature/auth/login/SnsLoginForm.tsx
@@ -3,37 +3,39 @@
 import Image from 'next/image'
 
 import { discordIcon, googleLogo } from '@/assets'
+import {
+  API_PATH,
+  BACK_API_URL,
+  GOOGLE_CLIENT_ID_URL,
+} from '@/constants/apiPath'
 import { useToast } from '@/hooks'
 
 /**
  * SNS 로그인 폼 컴포넌트
  */
 export default function SnsLoginForm() {
+  const redirectUri = `${BACK_API_URL}${API_PATH.GOOGLE_LOGIN_API_PATH}`
+
+  const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID_URL}&redirect_uri=${redirectUri}&response_type=code&scope=email profile`
+
   const { triggerToast } = useToast()
-  const handleGoogleLogin = async () => {
-    /**
-     * TODO: 구글 로그인 API 연동
-     */
-    triggerToast('success', '구글 로그인 성공')
-  }
 
   const handleDiscordLogin = async () => {
     /**
-     * TODO: 구글 로그인 API 연동
+     * TODO: 디스코드 로그인 API 연동
      */
 
     triggerToast('success', '디스코드 로그인 성공')
   }
   return (
     <div className="flex items-center justify-center gap-5">
-      <button type="button">
+      <a href={googleLoginUrl}>
         <Image
           src={googleLogo}
           alt="googleIcon"
           className="w-13 cursor-pointer"
-          onClick={handleGoogleLogin}
         />
-      </button>
+      </a>
       <button type="button">
         <Image
           src={discordIcon}
