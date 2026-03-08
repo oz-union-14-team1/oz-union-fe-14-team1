48e77735f2b7acd0dcb7cf0b8a63dc4b2e559f51
LeeSaemMool <sammul95@gmail.com>
2026-03-08 19:25:53 +0900

    feat: 필터 UI에 사용되는 공통컴포넌트 추가 및 수정 (#176)
    - Button 배경색 없는 empty variant 추가
    - shadcn checkbox, slider 컴포넌트 추가
    - 필터 디자인 토큰(shadow, 색상) token.css에 추가
    - PLATFORM_OPTIONS 상수 추가

---
+++ src/components/ui/slider.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Slider as SliderPrimitive } from 'radix-ui'
+import * as React from 'react'
+
+import { cn } from '@/utils'
+
+function Slider({
+  className,
+  defaultValue,
+  value,
+  min = 0,
+  max = 100,
+  ...props
+}: React.ComponentProps<typeof SliderPrimitive.Root>) {
+  const _values = React.useMemo(
+    () =>
+      Array.isArray(value)
+        ? value
+        : Array.isArray(defaultValue)
+          ? defaultValue
+          : [min, max],
+    [value, defaultValue, min, max]
+  )
+
+  return (
+    <>
+      <SliderPrimitive.Root
+        data-slot="slider"
+        defaultValue={defaultValue}
+        value={value}
+        min={min}
+        max={max}
+        className={cn(
+          'relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
+          className
+        )}
+        {...props}
+      >
+        <SliderPrimitive.Track
+          data-slot="slider-track"
+          className={cn(
+            (className =
+              'relative grow overflow-hidden rounded-full bg-surface-rangebar data-[orientation=horizontal]:h-1.5 data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5')
+          )}
+        >
+          <SliderPrimitive.Range
+            data-slot="slider-range"
+            className={cn(
+              'absolute bg-gradient-sub data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full'
+            )}
+          />
+        </SliderPrimitive.Track>
+        {Array.from({ length: _values.length }, (_, index) => (
+          <SliderPrimitive.Thumb
+            key={index}
+            data-slot="slider-thumb"
+            className="block size-2.5 shrink-0 rounded-full border border-white bg-white shadow-sm transition-[color,box-shadow] focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+          />
+        ))}
+      </SliderPrimitive.Root>
+      <div className="mt-1 flex justify-end">
+        <span className="text-[10px] text-btn-disabled-text">{_values[0]}</span>
+      </div>
+    </>
+  )
+}
+
+export { Slider }
