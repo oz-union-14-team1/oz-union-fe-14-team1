5c0591a2ca8b379216a7478a02792378d5d32a4e
LeeSaemMool <sammul95@gmail.com>
2026-03-24 18:29:51 +0900

    feat: 배너 이미지 추가 및 다크 오버레이 추가 (#304)
    - 배너 이미지 추가 및 변경
    - 다크 오버레이 추가

--- src/components/feature/community/CommunityBannerItem.tsx
+++ src/components/feature/community/CommunityBannerItem.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
-import { ROUTES_PATHS } from '@/constants'
 import { Banner } from '@/types/banner'
 
 import SectionNav from './SectionNav'
@@ -24,25 +22,20 @@ export default function CommunityBannerItem({
 }: CommunityBannerItemProps) {
   return (
     <div className="relative h-110 min-h-110 w-full shrink-0 snap-start overflow-hidden">
-      <Link
-        href={ROUTES_PATHS.GAME_DETAIL(banner.id)}
-        className="absolute inset-0"
-      >
-        <Image
-          src={banner.imgUrl}
-          alt={banner.title}
-          fill
-          priority
-          sizes="(min-width: 1024px) 66vw, 100vw"
-          className="pointer-events-none object-cover object-top"
-        />
-        <div className="absolute right-0 bottom-0 left-0 bg-linear-to-t from-black/60 to-transparent p-6">
-          <h2 className="line-clamp-2 text-4xl font-semibold text-white">
-            {banner.title}
-          </h2>
-        </div>
-      </Link>
-
+      <Image
+        src={banner.imgUrl}
+        alt={banner.title}
+        fill
+        priority
+        sizes="(min-width: 1024px) 66vw, 100vw"
+        className="pointer-events-none object-cover"
+      />
+      <div className="absolute inset-0 bg-black/20" />
+      <div className="absolute right-0 bottom-0 left-0 bg-linear-to-t from-black/60 to-transparent p-6">
+        <h2 className="line-clamp-2 text-4xl font-semibold text-white">
+          {banner.title}
+        </h2>
+      </div>
       <SectionNav
         direction="prev"
         disabled={currentPage === 0}
